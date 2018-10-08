821a8e09f68edbbd91e80debeb3e2e9fabe88c9b
c42-arun <arun.kumar.rs@gmail.com>
2018-10-08 09:04:07 +0100

    Added jquery to source

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function selectAvatar(event) {
 
 function newAvatar(event) {
     if (fileChange(event)) {
+        const currentAvatar = $("#currentAvatar");
         currentAvatar.hide();
         $("#avatarInstructions").show();
     }
